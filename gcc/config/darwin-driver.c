7a2c8794a0280970a11d77e1897583d1c87d16e2
Iain Sandoe <iain@sandoe.co.uk>
2020-08-03 22:46:40 +0100

    Darwin, Arm64 : Base build changes to Darwin common code.
    It seems that the arm64 port will not make use of traditional mach-o
    pic but instead use a GOT - as such, the target-dependent parts of
    darwin.c are made NOP for this.  We still expect to apply other Mach-O
    constraints.
    
    Most of the change is about the support for giving the user an error
    message when they try to invoke the compiler with multiple -arch
    flags.
    
    There is no provision in the initial port to handle the arm64_32 variant.
    This is (understood to be) only used in watchOS which we are not
    supporting initially.
    
    +  No machopic indirections for Arm64 personality/LSDA entries.
    
    As for x86-64 (64bit code), the personality and LSDA .cfi_xxx entries
    do not require an indirection in the compiler code.
    
    + Accept arm64 in arch flags, for aarch64 compilers.
    
    We were checking for this and complaining if it was present for an
    X86 or PPC compiler, but we also need to accept (and ignore it) for
    an aarch64 / Arm64 compiler.

--- gcc/config/darwin-driver.c
+++ gcc/config/darwin-driver.c
@@ -277,10 +277,13 @@ darwin_driver_init (unsigned int *decoded_options_count,
   bool seenX86_64 = false;
   bool seenPPC = false;
   bool seenPPC64 = false;
+#if !DARWIN_ARM64
+  bool seenArm64 = false;
   bool seenM32 = false;
   bool seenM64 = false;
   bool appendM32 = false;
   bool appendM64 = false;
+#endif
   const char *vers_string = NULL;
   bool seen_version_min = false;
   bool seen_sysroot_p = false;
@@ -305,6 +308,12 @@ darwin_driver_init (unsigned int *decoded_options_count,
 	    seenPPC = true;
 	  else if (!strcmp ((*decoded_options)[i].arg, "ppc64"))
 	    seenPPC64 = true;
+	  else if (!strcmp ((*decoded_options)[i].arg, "arm64"))
+#if !DARWIN_ARM64
+	    seenArm64 = true;
+#else
+	    ; /* We accept the option, but don't need to act on it.  */
+#endif
 	  else
 	    error ("this compiler does not support %qs",
 		   (*decoded_options)[i].arg);
@@ -318,7 +327,7 @@ darwin_driver_init (unsigned int *decoded_options_count,
 	  --i;
 	  --*decoded_options_count; 
 	  break;
-
+#if !DARWIN_ARM64
 	case OPT_m32:
 	  seenM32 = true;
 	  break;
@@ -326,7 +335,7 @@ darwin_driver_init (unsigned int *decoded_options_count,
 	case OPT_m64:
 	  seenM64 = true;
 	  break;
-
+#endif
 	case OPT_filelist:
 	case OPT_framework:
 	  ++*decoded_options_count;
@@ -392,6 +401,9 @@ darwin_driver_init (unsigned int *decoded_options_count,
   if (seenPPC || seenPPC64)
     warning (0, "this compiler does not support %<PowerPC%>"
 	     " (arch flags ignored)");
+  else if (seenArm64)
+    warning (0, "this compiler does not support %<Arm64%>"
+	     " (arch flags ignored)");
   if (seenX86)
     {
       if (seenX86_64 || seenM64)
@@ -411,6 +423,8 @@ darwin_driver_init (unsigned int *decoded_options_count,
 #elif DARWIN_PPC
   if (seenX86 || seenX86_64)
     warning (0, "this compiler does not support %<x86%> (arch flags ignored)");
+  else if (seenArm64)
+    warning (0, "this compiler does not support %<Arm64%> (arch flags ignored)");
   if (seenPPC)
     {
       if (seenPPC64 || seenM64)
@@ -427,8 +441,14 @@ darwin_driver_init (unsigned int *decoded_options_count,
       else if (! seenM64) /* Add -m64 if the User didn't. */
 	appendM64 = true;
     }
+#elif DARWIN_ARM64
+  if (seenPPC || seenPPC64)
+    warning (0, "this compiler does not support %<PowerPC%> (arch flags ignored)");
+  if (seenX86 || seenX86_64)
+    warning (0, "this compiler does not support %<X86%> (arch flags ignored)");
 #endif
 
+#if !DARWIN_ARM64
   if (appendM32 || appendM64)
     {
       ++*decoded_options_count;
@@ -438,6 +458,7 @@ darwin_driver_init (unsigned int *decoded_options_count,
       generate_option (appendM32 ? OPT_m32 : OPT_m64, NULL, 1, CL_DRIVER,
 		       &(*decoded_options)[*decoded_options_count - 1]);
     }
+#endif
 
   if (! seen_sysroot_p)
     {
--- gcc/config/darwin.c
+++ gcc/config/darwin.c
@@ -116,7 +116,7 @@ static bool ld_init_term_start_labels = false;
 section * darwin_sections[NUM_DARWIN_SECTIONS];
 
 /* While we transition to using in-tests instead of ifdef'd code.  */
-#if !HAVE_lo_sum
+#if !HAVE_lo_sum || DARWIN_ARM64
 #define gen_macho_high(m,a,b) (a)
 #define gen_macho_low(m,a,b,c) (a)
 #endif
@@ -1050,6 +1050,7 @@ machopic_legitimize_pic_address (rtx orig, machine_mode mode, rtx reg)
   return pic_ref;
 }
 
+#if !DARWIN_ARM64
 /* Callbacks to output the stub or non-lazy pointers.
    Each works on the item in *SLOT,if it has been used.
    DATA is the FILE* for assembly output.
@@ -1205,6 +1206,7 @@ machopic_finish (FILE *asm_out_file)
   machopic_indirections->traverse_noresize
     <FILE *, machopic_output_indirection> (asm_out_file);
 }
+#endif
 
 int
 machopic_operand_p (rtx op)
@@ -2236,6 +2238,8 @@ darwin_emit_except_table_label (FILE *file)
 rtx
 darwin_make_eh_symbol_indirect (rtx orig, bool ARG_UNUSED (pubvis))
 {
+  if (DARWIN_ARM64)
+    return orig;
   if (DARWIN_PPC == 0 && TARGET_64BIT)
     return orig;
 
@@ -3047,7 +3051,12 @@ darwin_file_end (void)
       fprintf (asm_out_file, "\t.long\t0\n\t.long\t%u\n", flags);
      }
 
+#if !DARWIN_ARM64
   machopic_finish (asm_out_file);
+#else
+  gcc_checking_assert (!machopic_indirections);
+#endif
+
   if (flag_apple_kext)
     {
       /* These sections are only used for kernel code.  */
