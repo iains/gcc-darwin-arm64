0ef518e1a405f1d0d45b6375e014484851ba62a0
Iain Sandoe <iain@sandoe.co.uk>
2020-08-16 12:11:37 +0100

    aarch64, Darwin, libgcc: Add support for aarch64 darwin.
    1. libgcc, aarch64, Darwin: Initial libgcc boiler-plate changes.
    
    Initial changes to build libgcc for aarch64 Darwin (declare the port,
    handle Mach-O assembler syntax and arrange for building the main extra
    pieces).
    
    2. libgcc, aarch64, Darwin: Do not build legacy EH shim.
    
    Darwin aarch64 does not exist for macOS 10.6, so there is no need to
    build a shim to target it.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    
    libgcc/
    
    	* config.host: Omit the Darwin10 unwinder shim for aarch64 Darwin.
    
    3. libgcc, aarch64, Darwin: Darwin should use an OS call to clear caches.
    
    The sys_icache_invalidate() function is the right one to use.
    
    The fixes issue #17
    
    4. libgcc, aarch64, Darwin: Enable heap trampolines.

--- gcc/config/aarch64/aarch64.cc
+++ gcc/config/aarch64/aarch64.cc
@@ -28575,7 +28575,7 @@ aarch64_libgcc_floating_mode_supported_p
 
 /* The architecture reserves bits 0 and 1 so use bit 2 for descriptors.  */
 #undef TARGET_CUSTOM_FUNCTION_DESCRIPTORS
-#define TARGET_CUSTOM_FUNCTION_DESCRIPTORS 4
+#define TARGET_CUSTOM_FUNCTION_DESCRIPTORS AARCH64_CUSTOM_FUNCTION_TEST
 
 #undef TARGET_HARD_REGNO_NREGS
 #define TARGET_HARD_REGNO_NREGS aarch64_hard_regno_nregs
