3a5fca6f1dc4d4e3d81c69a8fbad1702395e5462
Iain Sandoe <iain@sandoe.co.uk>
2020-08-19 19:16:43 +0100

    Darwin, Arm64 : Implement darwinpcs D.2
    This allows us to pass items smaller than 8 bytes with their natural
    alignment packed as appropriate into the stack.
    
    This means that multiple args can use one AAPCS64 stack slot.
    
    We continue to account the stack in terms of words, but for darwinpcs
    the last word in the cumulative arguments is filled incrementally.
    
    So the allocation will remain 8-byte-wise, but the filling can be 1-byte
    aligned.
    
    This also means that darwinpcs needs to override the standard hook
    for TARGET_FUNCTION_ARG_ROUND_BOUNDARY.

--- gcc/config/aarch64/aarch64.h
+++ gcc/config/aarch64/aarch64.h
@@ -58,7 +58,8 @@
 #define TARGET_SIMD (!TARGET_GENERAL_REGS_ONLY && AARCH64_ISA_SIMD)
 #define TARGET_FLOAT (!TARGET_GENERAL_REGS_ONLY && AARCH64_ISA_FP)
 
-/* Object format, ABI and syntax for Darwin platforms.  */
+/* If this is non-zero then generated code of the object format, ABI and
+   assembler syntax used by Darwin (Mach-O) platforms.  */
 #define TARGET_MACHO		0
 
 #define UNITS_PER_WORD		8
@@ -976,6 +977,12 @@ typedef struct
 				   aapcs_reg == NULL_RTX.  */
   int aapcs_stack_size;		/* The total size (in words, per 8 byte) of the
 				   stack arg area so far.  */
+  int darwinpcs_stack_bytes;	/* If the argument is passed on the stack, this
+				   the byte-size.  */
+  int darwinpcs_sub_word_offset;/* This is the offset of this arg within a word
+				   when placing smaller items for darwinpcs.  */
+  int darwinpcs_sub_word_pos;	/* The next byte available within the word for
+				   darwinpcs.  */
   bool silent_p;		/* True if we should act silently, rather than
 				   raise an error for invalid calls.  */
 } CUMULATIVE_ARGS;
