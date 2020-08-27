9ab6e7109d75e56fcfedc9536babdb3b112e53cc
Iain Sandoe <iain@sandoe.co.uk>
2020-08-27 19:44:45 +0100

    Darwin, Arm64 : Avoid negative offsets in PAGE relocs.
    It's not clear to me whether this is a mistake in the LLVM backend
    or a genuine restriction in the relocation (the wording of the addend
    reloc says neither 'signed' nor 'unsigned'.  This works around this
    by making known negative offsets non-legitimate and composing
    the address by:
      ardp foo@PAGE
      add dest, temp, foo@PAGEOFF
      add dest, dest, neg_offset

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -3007,6 +3007,21 @@ aarch64_load_symref_appropriately (rtx dest, rtx imm,
 	if (can_create_pseudo_p ())
 	  tmp_reg = gen_reg_rtx (mode);
 
+	if (TARGET_MACHO)
+	  {
+	    rtx sym, off;
+	    split_const (imm, &sym, &off);
+	    /* Negative offsets don't work, whether by intention is TBD.  */
+	    if (INTVAL (off) < 0)
+	      {
+		emit_move_insn (tmp_reg, gen_rtx_HIGH (mode, sym));
+		emit_insn (gen_add_losym (dest, tmp_reg, sym));
+		/* FIXME: add the SI option if/when we support ilp32.  */
+		emit_insn (gen_adddi3 (dest, dest, off));
+		return;
+	      }
+	   /* else positive offset is OK.  */
+	  }
 	emit_move_insn (tmp_reg, gen_rtx_HIGH (mode, imm));
 	emit_insn (gen_add_losym (dest, tmp_reg, imm));
 	return;
@@ -9331,6 +9346,7 @@ aarch64_classify_address (struct aarch64_address_info *info,
       /* load literal: pc-relative constant pool entry.  Only supported
          for SI mode or larger.  */
       info->type = ADDRESS_SYMBOLIC;
+      info->offset = NULL_RTX;
 
       if (!load_store_pair_p
 	  && GET_MODE_SIZE (mode).is_constant (&const_size)
@@ -9339,6 +9355,10 @@ aarch64_classify_address (struct aarch64_address_info *info,
 	  rtx sym, addend;
 
 	  split_const (x, &sym, &addend);
+	  if (TARGET_MACHO && INTVAL (addend) < 0)
+	    /* ld64 doesn't like sym-offset, it looks like a violation of atom
+	       rules.  */
+	    return false;
 	  return ((GET_CODE (sym) == LABEL_REF
 		   || (GET_CODE (sym) == SYMBOL_REF
 		       && CONSTANT_POOL_ADDRESS_P (sym)
@@ -9355,6 +9375,8 @@ aarch64_classify_address (struct aarch64_address_info *info,
 	{
 	  rtx sym, offs;
 	  split_const (info->offset, &sym, &offs);
+	  if (TARGET_MACHO && INTVAL (offs) < 0)
+	    return false;
 	  if (GET_CODE (sym) == SYMBOL_REF
 	      && (aarch64_classify_symbol (sym, INTVAL (offs))
 		  == SYMBOL_SMALL_ABSOLUTE))
