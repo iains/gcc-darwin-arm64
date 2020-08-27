ad92fe67d0f47bb7d98f5c773aa801a2e2ea1e20
Iain Sandoe <iain@sandoe.co.uk>
2020-08-27 19:39:10 +0100

    Darwin, Arm64 : Make code label references use PAGE relocs
    We need to be able to refer to Lxxxx labels in libgnat.

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -10885,6 +10885,7 @@ output_macho_postfix_expr (FILE *file, rtx x, const char *postfix)
     case CODE_LABEL:
       ASM_GENERATE_INTERNAL_LABEL (buf, "L", CODE_LABEL_NUMBER (x));
       assemble_name (file, buf);
+      fprintf (file, "@%s", postfix);
       break;
 
     case CONST_INT:
@@ -18441,7 +18442,9 @@ aarch64_classify_symbol (rtx x, HOST_WIDE_INT offset)
 	case AARCH64_CMODEL_SMALL_SPIC:
 	case AARCH64_CMODEL_SMALL_PIC:
 	case AARCH64_CMODEL_SMALL:
-	  return SYMBOL_SMALL_ABSOLUTE;
+	  return TARGET_MACHO
+		 ? SYMBOL_MO_SMALL_PCR
+		 : SYMBOL_SMALL_ABSOLUTE;
 
 	default:
 	  gcc_unreachable ();
