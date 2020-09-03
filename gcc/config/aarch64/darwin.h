b1affd208176e777f1b4a96dd97d87a977b336fe
Iain Sandoe <iain@sandoe.co.uk>
2020-09-03 19:49:39 +0100

    Darwin, Arm64 : Darwin should use an OS call to clear caches.
    The sys_icache_invalidate() function is the right one to use.
    
    The fixes issue #17

--- gcc/config/aarch64/darwin.h
+++ gcc/config/aarch64/darwin.h
@@ -229,3 +229,8 @@ along with GCC; see the file COPYING3.  If not see
 
 #undef ASM_OUTPUT_DEF_FROM_DECLS
 
+#undef CLEAR_INSN_CACHE
+#define CLEAR_INSN_CACHE(beg, end)				\
+  extern void sys_icache_invalidate(void *start, size_t len);	\
+  sys_icache_invalidate ((beg), (size_t)((end)-(beg)))
+
