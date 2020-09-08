d6e88ec8f7b2ba621ed04ec2dd6d4955b5e447d6
Iain Sandoe <iain@sandoe.co.uk>
2020-09-08 19:50:55 +0100

    Darwin, Arm64 : We are now handling negative addends.
    Part of the code attempting to deal with the "PAGE-N" issue
    was to reject negative offsets in the legitimizers - actually, this
    wasn't effective anyway.  We've now got code catching the cases
    and using a separate sum instruction, so delete these.

--- gcc/config/aarch64/aarch64.c
+++ gcc/config/aarch64/aarch64.c
@@ -9426,10 +9426,6 @@ aarch64_classify_address (struct aarch64_address_info *info,
 	  poly_int64 offset;
 	  rtx sym = strip_offset_and_salt (x, &offset);
 
-	  if (TARGET_MACHO && INTVAL (offset) < 0)
-	    /* ld64 doesn't like sym-offset, it looks like a violation of atom
-	       rules.  */
-	    return false;
 	  return ((LABEL_REF_P (sym)
 		   || (SYMBOL_REF_P (sym)
 		       && CONSTANT_POOL_ADDRESS_P (sym)
@@ -9448,8 +9444,6 @@ aarch64_classify_address (struct aarch64_address_info *info,
 	  HOST_WIDE_INT const_offset;
 	  rtx sym = strip_offset_and_salt (info->offset, &offset);
 
-	  if (TARGET_MACHO && INTVAL (offset) < 0)
-	    return false;
 	  if (SYMBOL_REF_P (sym)
 	      && offset.is_constant (&const_offset)
 	      && (aarch64_classify_symbol (sym, const_offset)
