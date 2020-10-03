7b09bf57f223e433b6449218bcb69901ff51bfbc
Iain Sandoe <iain@sandoe.co.uk>
2020-10-03 13:40:03 +0100

    Objective-C, Darwin : Update metadata section uses.
    Newer versions of ld64 are more picky about adherence to placement
    rules for objective c metadata.  This adds protocol refs and uses
    the ivar refs for all targets.
    
    gcc/ChangeLog:
    
    	* config/darwin-sections.def (objc2_data_section): New.
    	(objc2_ivar_section): New.
    	* config/darwin.c (darwin_objc2_section): Act on Protocol and
    	ivar refs.
    
    gcc/objc/ChangeLog:
    
    	* objc-next-runtime-abi-02.c
    	(next_runtime_abi_02_init_metadata_attributes): Make protocol
    	refs a distinct section.

--- gcc/config/darwin.c
+++ gcc/config/darwin.c
@@ -1461,6 +1461,9 @@ darwin_objc2_section (tree decl ATTRIBUTE_UNUSED, tree meta, section * base)
 
   else if (!strncmp (p, "G2_META", 7) || !strncmp (p, "G2_CLAS", 7))
     return darwin_sections[objc2_classdefs_section];
+  else if (!strncmp (p, "V2_PCOL", 7))
+    return ld_uses_coal_sects ? darwin_sections[data_coal_section]
+                              : darwin_sections[objc2_data_section];
   else if (!strncmp (p, "V2_MREF", 7))
     return darwin_sections[objc2_message_refs_section];
   else if (!strncmp (p, "V2_CLRF", 7))
@@ -1495,6 +1498,9 @@ darwin_objc2_section (tree decl ATTRIBUTE_UNUSED, tree meta, section * base)
 
   else if (!strncmp (p, "V2_CSTR", 7))
     return darwin_sections[objc2_constant_string_object_section];
+
+  else if (!strncmp (p, "V2_IVRF", 7))
+    return darwin_sections[objc2_ivar_section];
 
   /* Not recognized, default.  */
   return base;
