fc13b75e62e563e5429744c88a368cff329aee4c
Iain Sandoe <iain@sandoe.co.uk>
2020-10-22 09:30:54 +0100

    testsuite, Darwin, Arm64 : Skip more tests using 'check-function-bodies'.
    The implementation for check-function-bodies doesn't support Mach-O at this
    point - so this is justy noise.  TODO: provide a Mach-O version of the scan
    asm function.

--- gcc/testsuite/gcc.target/aarch64/advsimd-intrinsics/bfdot-2.c
+++ gcc/testsuite/gcc.target/aarch64/advsimd-intrinsics/bfdot-2.c
@@ -3,7 +3,7 @@
 /* { dg-require-effective-target arm_v8_2a_bf16_neon_ok } */
 /* { dg-add-options arm_v8_2a_bf16_neon }  */
 /* { dg-additional-options "-mbig-endian --save-temps" } */
-/* { dg-final { check-function-bodies "**" "" {-O[^0]} } } */
+/* { dg-final { check-function-bodies "**" "" {-O[^0]} { target { ! aarch64*-*-darwin* } } } } */
 /* { dg-skip-if "" { *-*-* } { "-fno-fat-lto-objects" } } */
 
 #include <arm_neon.h>
