3a65301cf5a95a71ef0c78deabb104a60a59aeeb
Iain Sandoe <iain@sandoe.co.uk>
2021-12-08 13:02:32 +0000

    aarch64, Darwin: Amend float128 additions to correct max_align_t.
    The alignment of floating types is tested in two places, one
    internal to GCC and in the stddef header.
    
    The first checks the global float128_type_node so we should assign
    our decl to that node.
    
    For the second, the simple approach is to special case the Darwin
    aarch64 port (in addition to i386).  TODO: see if there is a more
    generic way to do this.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-builtins.c (aarch64_init_float128_types):
    	* config/aarch64/aarch64.c (aarch64_gimplify_va_arg_expr):
    	* config/aarch64/aarch64.h (GTY):
    	* ginclude/stddef.h (defined):

--- gcc/config/aarch64/aarch64.h
+++ gcc/config/aarch64/aarch64.h
@@ -1300,9 +1300,8 @@ extern GTY(()) tree aarch64_fp16_ptr_type_node;
 extern GTY(()) tree aarch64_bf16_type_node;
 extern GTY(()) tree aarch64_bf16_ptr_type_node;
 
-/* This type is the user-visible __float128 (on Mach-O), and a pointer to
-    that type.  Defined in aarch64-builtins.c.  */
-extern GTY(()) tree aarch64_float128_type_node;
+/* A pointer to the user-visible __float128 (on Mach-O).  Defined in
+   aarch64-builtins.c.  */
 extern GTY(()) tree aarch64_float128_ptr_type_node;
 
 /* The generic unwind code in libgcc does not initialize the frame pointer.
