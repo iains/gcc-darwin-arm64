195a4aeed8d36f0c3e2edb509e650bf4ad42a631
Iain Sandoe <iain@sandoe.co.uk>
2022-07-26 12:17:42 +0100

    c++, tls init: Temporary support for cross-tu static initialization [PR106435].
    This patch set has been reviewed and there are suggestions for improvements
    or a revised version.  However, in the short-term this is a usable placeholder.
    
    ===
    
    1/ Currently, cross-tu static initialisation is not supported for targets without
    alias support.
    
    This patch adds support by building a stub function in place of the alias for
    such targets.  Although this is slightly less efficient, it is a matter of
    correcting wrong code and, for targets supporting sibcalls, actually the
    penalty is a single jump for optimised code.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    
    	PR c++/106435
    
    gcc/c-family/ChangeLog:
    
    	* c-opts.cc (c_common_post_options): Allow fextern-tls-init for targets
    	without alias support.
    
    gcc/cp/ChangeLog:
    
    	* decl2.cc (get_tls_init_fn): Allow targets without alias support.
    	(handle_tls_init): Emit aliases for single init functions where the
    	target supporst this, otherwise emit a stub function that calls the
    	main tls init function.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/cpp0x/pr106435-b.cc: New test.
    	* g++.dg/cpp0x/pr106435.C: New test.
    	* g++.dg/cpp0x/pr106435.h: New test.
    
    2/ C++, TLS, Darwin: Work around Darwin weak ref handling.
    
    In order to handle cross-TU dynamic initialization of thread local variables,
    we generate wrapper functions that call to the handler for the variable init.
    
    Those inits are guarded so that they only run once.
    
    From the perspective of a TU referencing a TLS variable, there is no way to
    determine if it requires a guarded dynamic init.  So we make a weak reference
    to said init and test for it at runtime.
    
    This is fine at runtime for Darwin, but fails at link time when we have a TLS
    var which does NOT need a guarded dynamic init (because then there is no init
    and Darwin's ld64 does not permit undefined weak references at link time).
    
    This patch is a work-around;  In the TU that is referencing the var, we now
    generate both the wrapper _and_ a weak definition of a dummy init function.
    
    So, in the case that there _is_ a dynamic init, the weak copy will be over-
    ridden by the linker.  In the case that we have a trivial static init the
    weak defined dummy init will be used.
    
    TODO: rework this code-gen path to remove the test for the function (it will
    always be true) since the combination of static and dynamic linkers will now
    be responsible for correct operation.

--- gcc/c-family/c-opts.cc
+++ gcc/c-family/c-opts.cc
@@ -1190,7 +1190,7 @@ c_common_post_options (const char **pfilename)
 
   if (flag_extern_tls_init)
     {
-      if (!TARGET_SUPPORTS_ALIASES || !SUPPORTS_WEAK)
+      if (!SUPPORTS_WEAK)
 	{
 	  /* Lazy TLS initialization for a variable in another TU requires
 	     alias and weak reference support.  */
