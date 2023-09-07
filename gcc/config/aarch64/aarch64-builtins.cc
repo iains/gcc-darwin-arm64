71e9e94b041709c9d2d4c361677480872e0a081d
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2023-09-07 07:33:42 +0200

    aarch64, Darwin: Support IEEE754 Quad Precision as __float128.
    aarch64-apple-darwin has a C long double type which is the same as the
    double. In order to support a higher precision floating point type,
    similarly to x86_64-apple-darwin, we define define __float128 (and
    _Float128).
    
    Co-authored-by: Iain Sandoe <iain@sandoe.co.uk>
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-builtins.cc (AARCH64_BUILTIN_HUGE_VALQ,
    	AARCH64_BUILTIN_INFQ): New built-ins.
    	(aarch64_init_float128_types): New function...
    	(aarch64_general_init_builtins): ... called here.
    	(aarch64_general_fold_builtin): Initialize the new built-ins.
    	* config/aarch64/aarch64.cc (aarch64_gimplify_va_arg_expr):
    	Handle __float128 on darwin.
    	(aarch64_mangle_type): Mangle __float128.
    	(aarch64_c_mode_for_suffix): Assign suffix q to __float128.
    	(aarch64_scalar_mode_supported_p): Ensure TFmode is allowed.
    	* config/aarch64/aarch64.h (aarch64_float128_ptr_type_node): New
    	node.
    	* ginclude/stddef.h (__max_align_f128): Ensure it has correct
    	alignment.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.target/aarch64/float128-darwin-1.C: New test.
    	* gcc.target/aarch64/darwin/float128-01.c: New test.
    
    libgcc/ChangeLog:
    
    	* config.host: Enable soft-float.
    	* config/aarch64/sfp-machine.h: Provide definitions for
    	aarch64-apple-darwin.
    	* config/aarch64/t-darwin: Fix comment.

--- gcc/config/aarch64/aarch64-builtins.cc
+++ gcc/config/aarch64/aarch64-builtins.cc
@@ -1023,6 +1023,9 @@ tree aarch64_fp16_ptr_type_node = NULL_TREE;
 /* Back-end node type for brain float (bfloat) types.  */
 tree aarch64_bf16_ptr_type_node = NULL_TREE;
 
+/* Pointer to __float128 on Mach-O, where the 128b float is not long double.  */
+tree aarch64_float128_ptr_type_node = NULL_TREE;
+
 /* Wrapper around add_builtin_function.  NAME is the name of the built-in
    function, TYPE is the function type, CODE is the function subcode
    (relative to AARCH64_BUILTIN_GENERAL), and ATTRS is the function
@@ -2033,6 +2036,29 @@ aarch64_init_bf16_types (void)
   aarch64_bf16_ptr_type_node = build_pointer_type (bfloat16_type_node);
 }
 
+/* Initialize the backend REAL_TYPE type supporting __float128 on Mach-O,
+   as well as the related built-ins.  */
+static void
+aarch64_init_float128_types (void)
+{
+  /* The __float128 type.  The node has already been created as
+     _Float128, so for C we only need to register the __float128 name for
+     it.  For C++, we create a distinct type which will mangle differently
+     (g) vs. _Float128 (DF128_) and behave backwards compatibly.  */
+  if (float128t_type_node == NULL_TREE)
+    {
+      float128t_type_node = make_node (REAL_TYPE);
+      TYPE_PRECISION (float128t_type_node)
+	= TYPE_PRECISION (float128_type_node);
+      SET_TYPE_MODE (float128t_type_node, TYPE_MODE (float128_type_node));
+      layout_type (float128t_type_node);
+    }
+  lang_hooks.types.register_builtin_type (float128t_type_node, "__float128");
+
+  aarch64_float128_ptr_type_node = build_pointer_type (float128t_type_node);
+}
+
+
 /* Pointer authentication builtins that will become NOP on legacy platform.
    Currently, these builtins are for internal use only (libgcc EH unwinder).  */
 
@@ -2444,8 +2470,9 @@ aarch64_general_init_builtins (void)
   aarch64_init_fp8_types ();
 
   aarch64_init_fp16_types ();
-
   aarch64_init_bf16_types ();
+  if (TARGET_MACHO)
+    aarch64_init_float128_types ();
 
   {
     aarch64_simd_switcher simd;
