e9be4d7bb9c5c2a72e391beb0eb3ff4b9a2cc3f8
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2023-09-07 07:33:42 +0200

    aarch64, Darwin: Support IEEE754 Quad Precision as __float128.
    aarch64-apple-darwin has a C long double type which is the same as the
    double. In order to support a higher precision floating point type,
    similarly to x86_64-apple-darwin, we define define __float128 (and
    _Float128).
    
    Co-authored-by: Iain Sandoe <iain@sandoe.co.uk>
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-builtins.cc (AARCH64_BUILTIN_HUGE_VALQ,
    	AARCH64_BUILTIN_INFQ): New built-ins.
    	(aarch64_init_float128_types): New function...
    	(aarch64_general_init_builtins): ... called here.
    	(aarch64_general_fold_builtin): Initialize the new built-ins.
    	* config/aarch64/aarch64.cc (aarch64_gimplify_va_arg_expr):
    	Handle __float128 on darwin.
    	(aarch64_mangle_type): Mangle __float128.
    	(aarch64_c_mode_for_suffix): Assign suffix q to __float128.
    	(aarch64_scalar_mode_supported_p): Ensure TFmode is allowed.
    	* config/aarch64/aarch64.h (aarch64_float128_ptr_type_node): New
    	node.
    	* ginclude/stddef.h (__max_align_f128): Ensure it has correct
    	alignment.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.target/aarch64/float128-darwin-1.C: New test.
    	* gcc.target/aarch64/darwin/float128-01.c: New test.
    
    libgcc/ChangeLog:
    
    	* config.host: Enable soft-float.
    	* config/aarch64/sfp-machine.h: Provide definitions for
    	aarch64-apple-darwin.
    	* config/aarch64/t-darwin: Fix comment.

--- gcc/config/aarch64/aarch64.h
+++ gcc/config/aarch64/aarch64.h
@@ -1574,6 +1574,10 @@ extern GTY(()) tree aarch64_fp16_ptr_type_node;
    bfloat16_type_node.  Defined in aarch64-builtins.cc.  */
 extern GTY(()) tree aarch64_bf16_ptr_type_node;
 
+/* A pointer to the user-visible __float128 (on Mach-O).  Defined in
+   aarch64-builtins.c.  */
+extern GTY(()) tree aarch64_float128_ptr_type_node;
+
 /* The generic unwind code in libgcc does not initialize the frame pointer.
    So in order to unwind a function using a frame pointer, the very first
    function that is unwound must save the frame pointer.  That way the frame
