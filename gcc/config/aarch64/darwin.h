f3fb2dc93675fa602e8684561466fb2b6f1ebaf2
Iain Sandoe <iain@sandoe.co.uk>
2023-09-30 17:39:08 +0100

    aarch64, Darwin: Pass march values to the assembler.
    All current aarch64 Darwin versions use 'clang -cc1as' as their assembler
    this means that we can safely pass -march to the assembler fixing Issue #115.
    
    gcc/ChangeLog:
    
    	* config/aarch64/darwin.h (ASM_SPEC): Pass -march values to the
    	assembler.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/config/aarch64/darwin.h
+++ gcc/config/aarch64/darwin.h
@@ -86,7 +86,7 @@ along with GCC; see the file COPYING3.  If not see
 "%{!mkernel:%{!static:-fPIC}} " DARWIN_CC1_SPEC
 
 #undef ASM_SPEC
-#define ASM_SPEC "-arch %(darwin_arch) "\
+#define ASM_SPEC "-arch %(darwin_arch) %{march*} %{mtune*} "\
   ASM_OPTIONS " %{static} " ASM_MMACOSX_VERSION_MIN_SPEC
 
 #undef ENDFILE_SPEC
