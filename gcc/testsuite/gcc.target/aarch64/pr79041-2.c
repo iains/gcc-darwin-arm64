c6588e055c61d3d0b687362e60a7a0fd52f1048d
Iain Sandoe <iain@sandoe.co.uk>
2023-10-14 14:38:35 +0100

    testsuite,aarch64,Darwin: XFAIL because if large code model.
    mcmodel=large is on the TODO, until then we expect this to fail.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/pr63304_1.c: XFAIL for Darwin.
    	* gcc.target/aarch64/pr78733.c: XFAIL for Darwin.
    	* gcc.target/aarch64/pr79041-2.c: Likewise.
    	* gcc.target/aarch64/pr63304_1.c: XFAIL Darwin.
    	* gcc.target/aarch64/pr70120-2.c: Likewise.
    	* gcc.target/aarch64/pr78733.c: Likewise.
    	* gcc.target/aarch64/pr79041-2.c: Likewise.
    	* gcc.target/aarch64/pr94530.c: Likewise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/gcc.target/aarch64/pr79041-2.c
+++ gcc/testsuite/gcc.target/aarch64/pr79041-2.c
@@ -2,6 +2,7 @@
 /* { dg-options "-O2 -mcmodel=large -mpc-relative-literal-loads -fno-pie" } */
 /* { dg-require-effective-target lp64 } */
 /* { dg-skip-if "-mcmodel=large, no support for -fpic" { aarch64-*-* }  { "-fpic" } { "" } } */
+/* { dg-xfail-if "issue #100" { *-*-darwin* } } */
 
 __int128
 t (void)
@@ -9,5 +10,5 @@ t (void)
   return ((__int128)0x123456789abcdef << 64) | 0xfedcba987654321;
 }
 
-/* { dg-final { scan-assembler "adr" } } */
+/* { dg-final { scan-assembler "adr" { xfail { *-*-darwin* } } } } */
 /* { dg-final { scan-assembler-not "adrp" } } */
