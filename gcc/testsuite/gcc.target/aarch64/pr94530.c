77001ca78e5b2340a018d416d851221f11109b70
Iain Sandoe <iain@sandoe.co.uk>
2023-10-14 14:38:35 +0100

    testsuite,aarch64,Darwin: XFAIL because if large code model.
    mcmodel=large is on the TODO, until then we expect this to fail.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/pr63304_1.c: XFAIL for Darwin.
    	* gcc.target/aarch64/pr78733.c: XFAIL for Darwin.
    	* gcc.target/aarch64/pr79041-2.c: Likewise.
    	* gcc.target/aarch64/pr63304_1.c: XFAIL Darwin.
    	* gcc.target/aarch64/pr70120-2.c: Likewise.
    	* gcc.target/aarch64/pr78733.c: Likewise.
    	* gcc.target/aarch64/pr79041-2.c: Likewise.
    	* gcc.target/aarch64/pr94530.c: Likewise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/gcc.target/aarch64/pr94530.c
+++ gcc/testsuite/gcc.target/aarch64/pr94530.c
@@ -1,6 +1,7 @@
 /* { dg-do compile } */
 /* { dg-require-effective-target lp64 } */
 /* { dg-options "-Os -mcpu=falkor -mpc-relative-literal-loads -mcmodel=large -fno-pie" } */
+/* { dg-xfail-if "issue #100" { *-*-darwin* } } */
 
 extern void bar(const char *);
 
