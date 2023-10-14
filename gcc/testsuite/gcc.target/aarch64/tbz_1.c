edd6d4332604be9d737d60ed40d7c5de7b1ab0fd
Iain Sandoe <iain@sandoe.co.uk>
2023-10-14 14:21:03 +0100

    testsuite, aarch64, Darwin: Allow unwind frames.
    Darwin's function body scan code needs the function start and end
    markers which are enabled by frames (either unwind or debug).  In the
    absence of these, it's not clear how we'd delinate functions, since
    Mach-O does not use .size.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/tbz_1.c: Allow unwind frames for Darwin.
    	* gcc.target/aarch64/tbz_2.c: Likewise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/gcc.target/aarch64/tbz_1.c
+++ gcc/testsuite/gcc.target/aarch64/tbz_1.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
-/* { dg-additional-options "-O2 -std=c99  -fno-unwind-tables -fno-asynchronous-unwind-tables" } */
+/* { dg-additional-options "-O2 -std=c99 " } */
+/* { dg-additional-options " -fno-unwind-tables -fno-asynchronous-unwind-tables" { target { ! *-*-darwin* } } } */
 /* { dg-final { check-function-bodies "**" "" "" { target { le } } } } */
 
 #include <stdbool.h>
@@ -10,7 +11,7 @@ void h(void);
 ** g1:
 ** 	tbnz	[wx][0-9]+, #?0, .L([0-9]+)
 ** 	ret
-**	...
+** 	...
 */
 void g1(bool x)
 {
@@ -21,7 +22,7 @@ void g1(bool x)
 /*
 ** g2:
 ** 	tbz	[wx][0-9]+, #?0, .L([0-9]+)
-** 	b	h
+** 	b	_?h
 **	...
 */
 void g2(bool x)
@@ -33,7 +34,7 @@ void g2(bool x)
 /*
 ** g3_ge:
 ** 	tbnz	w[0-9]+, #?31, .L[0-9]+
-** 	b	h
+** 	b	_?h
 **	...
 */
 void g3_ge(int x)
@@ -46,7 +47,7 @@ void g3_ge(int x)
 ** g3_gt:
 ** 	cmp	w[0-9]+, 0
 ** 	ble	.L[0-9]+
-** 	b	h
+** 	b	_?h
 **	...
 */
 void g3_gt(int x)
@@ -58,7 +59,7 @@ void g3_gt(int x)
 /*
 ** g3_lt:
 ** 	tbz	w[0-9]+, #?31, .L[0-9]+
-** 	b	h
+** 	b	_?h
 **	...
 */
 void g3_lt(int x)
@@ -71,7 +72,7 @@ void g3_lt(int x)
 ** g3_le:
 ** 	cmp	w[0-9]+, 0
 ** 	bgt	.L[0-9]+
-** 	b	h
+** 	b	_?h
 **	...
 */
 void g3_le(int x)
@@ -85,9 +86,9 @@ void g3_le(int x)
 ** 	mov	w[0-9]+, 65279
 ** 	tst	w[0-9]+, w[0-9]+
 ** 	beq	.L[0-9]+
-** 	b	h
+** 	b	_?h
 **	...
-*/ 
+*/
 void g5(int x)
 {
   if (__builtin_expect (x & 0xfeff, 1))
