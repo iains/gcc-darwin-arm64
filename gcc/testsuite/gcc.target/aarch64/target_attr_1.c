824daa69d6c698bd5f321d03425371c2456aeffa
Iain Sandoe <iain@sandoe.co.uk>
2023-10-14 15:38:45 +0100

    testsuite,aarch64,Darwin: XFAIL tests that track per function attrs.
    We have not implemented per-function tracking in Darwin so XFAIL these for
    now.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/target_attr_1.c: XFAIL for Darwin.
    	* gcc.target/aarch64/target_attr_15.c: Likewise.
    	* gcc.target/aarch64/target_attr_4.c: Likewise.
    	* gcc.target/aarch64/target_attr_7.c: Likewise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/gcc.target/aarch64/target_attr_1.c
+++ gcc/testsuite/gcc.target/aarch64/target_attr_1.c
@@ -1,6 +1,5 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -mcpu=thunderx -march=armv8-a -dA" } */
-
 /* Test that cpu attribute overrides the command-line -mcpu.  */
 
 __attribute__ ((target ("cpu=cortex-a72.cortex-a53")))
@@ -10,5 +9,5 @@ foo (int a)
   return a + 1;
 }
 
-/* { dg-final { scan-assembler "//.tune cortex-a72.cortex-a53" } } */
+/* { dg-final { scan-assembler "//.tune cortex-a72.cortex-a53" { xfail *-*-darwin* } } } */
 /* { dg-final { scan-assembler-not "thunderx" } } */
