e9b7b5d882d7e1c81327778922dd0af1ab15c584
Iain Sandoe <iain@sandoe.co.uk>
2023-10-20 09:49:42 +0100

    testsuite, aarch64: Handle optional +crc.
    We have two conflicting assembler bugs, one that requires +crc to be appended
    and one that does not so we have to make it optional.
    
    Some of these tests were intending to be testing that certain extensions are
    not emitted, added scan-assembler-not for those.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/options_set_1.c:i XFAIL for Darwin.
    	* gcc.target/aarch64/options_set_11.c: Make +crc optional.
    	* gcc.target/aarch64/options_set_12.c: Likewise.
    	* gcc.target/aarch64/options_set_13.c: Likewise.
    	* gcc.target/aarch64/options_set_14.c: Likewise.
    	* gcc.target/aarch64/options_set_15.c: Likewise.
    	* gcc.target/aarch64/options_set_16.c: Likewise.
    	* gcc.target/aarch64/options_set_17.c: Likewise.
    	* gcc.target/aarch64/options_set_18.c: Likewise.
    	* gcc.target/aarch64/options_set_19.c: Likewise.
    	* gcc.target/aarch64/options_set_2.c: Likewise.
    	* gcc.target/aarch64/options_set_20.c: Likewise.
    	* gcc.target/aarch64/options_set_21.c: Likewise.
    	* gcc.target/aarch64/options_set_22.c: Likewise.
    	* gcc.target/aarch64/options_set_23.c: Likewise.
    	* gcc.target/aarch64/options_set_24.c: Likewise.
    	* gcc.target/aarch64/options_set_25.c: Likewise.
    	* gcc.target/aarch64/options_set_26.c: Likewise.
    	* gcc.target/aarch64/options_set_3.c: Likewise.
    	* gcc.target/aarch64/options_set_4.c: Likewise.
    	* gcc.target/aarch64/options_set_5.c: Likewise.
    	* gcc.target/aarch64/options_set_6.c: Likewise.
    	* gcc.target/aarch64/options_set_7.c: Likewise.
    	* gcc.target/aarch64/options_set_8.c: Likewise.
    	* gcc.target/aarch64/options_set_9.c: Likewise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/gcc.target/aarch64/options_set_25.c
+++ gcc/testsuite/gcc.target/aarch64/options_set_25.c
@@ -6,6 +6,8 @@ int main ()
   return 0;
 }
 
-/* { dg-final { scan-assembler {\.arch armv8\.4-a\+crc\+fp16\n} } } */
+/* { dg-final { scan-assembler {\.arch armv8\.4-a\+crc\+fp16\n} { target { ! *-*-darwin* } } } } */
+/* { dg-final { scan-assembler {\.arch armv8\.4-a\+fp16} { target *-*-darwin* } } } */
+/* { dg-final { scan-assembler-not {\+fp16fml} } } */
 
  /* fp16 smallest set to emit.  */
