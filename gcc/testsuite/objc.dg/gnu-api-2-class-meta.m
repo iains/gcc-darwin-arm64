61acb5af8466155e1222323605284741d4380485
Iain Sandoe <iain@sandoe.co.uk>
2023-10-26 10:46:24 +0100

    testsuite, Darwin: Add support for Aarch64 Darwin.
    This is mostly mechanical changes to recognise the port triple and, in some
    instances different assembler directives.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/torture/darwin-cfstring-3.C: Add aarch64-darwin support.
    	* gcc.dg/builtin-apply2.c: Likewise.
    	* gcc.dg/cwsc1.c: Likewise.
    	* gcc.dg/pr26427.c: Likewise.
    	* gcc.dg/pubtypes-2.c: Likewise.
    	* gcc.dg/pubtypes-3.c: Likewise.
    	* gcc.dg/pubtypes-4.c: Likewise.
    	* gcc.dg/rtl/aarch64/big-endian-cse-1.c: Likewise.
    	* gcc.dg/tls/pr78796.c: Likewise.
    	* gcc.dg/torture/darwin-cfstring-3.c: Likewise.
    	* gcc.dg/torture/stackalign/builtin-apply-2.c: Likewise.
    	* gcc.dg/tree-ssa/stdarg-2.c: Likewise.
    	* gcc.dg/tree-ssa/stdarg-4.c: Likewise.
    	* gcc.dg/tree-ssa/stdarg-5.c: Likewise.
    	* gfortran.dg/pr95690.f90: Likewise.
    	* lib/target-supports.exp: Likewise.
    	* obj-c++.dg/gnu-api-2-class-meta.mm: Likewise.
    	* obj-c++.dg/gnu-api-2-class.mm: Likewise.
    	* obj-c++.dg/torture/strings/const-cfstring-4.mm: Likewise.
    	* obj-c++.dg/torture/strings/const-str-10.mm: Likewise.
    	* obj-c++.dg/torture/strings/const-str-11.mm: Likewise.
    	* obj-c++.dg/torture/strings/const-str-9.mm: Likewise.
    	* objc.dg/gnu-api-2-class-meta.m: Likewise.
    	* objc.dg/gnu-api-2-class.m: Likewise.
    	* objc.dg/torture/strings/const-cfstring-4.m: Likewise.
    	* objc.dg/torture/strings/const-str-10.m: Likewise.
    	* objc.dg/torture/strings/const-str-11.m: Likewise.
    	* objc.dg/torture/strings/const-str-9.m: Likewise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/objc.dg/gnu-api-2-class-meta.m
+++ gcc/testsuite/objc.dg/gnu-api-2-class-meta.m
@@ -19,6 +19,7 @@
 
 /* { dg-do run } */
 /* { dg-skip-if "No API#2 pre-Darwin9" { *-*-darwin[5-8]* } { "-fnext-runtime" } { "" } } */
+/* { dg-skip-if "API unsupported" { arm64*-*-darwin* aarch64*-*-darwin* } { "-fnext-runtime" } { "" } } */
 /* { dg-xfail-run-if "Needs OBJC2 ABI" { *-*-darwin* && { lp64 && { ! objc2 } } } { "-fnext-runtime" } { "" } } */
 /* { dg-additional-options "-Wno-objc-root-class" } */
 /* { dg-additional-options "-DOBJC_OLD_DISPATCH_PROTOTYPES" { target { *-*-darwin* } } } */
