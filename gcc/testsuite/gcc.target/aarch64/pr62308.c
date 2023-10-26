6a9a94b289ce6a327082f9a6033739ea1ecddcda
Iain Sandoe <iain@sandoe.co.uk>
2023-10-26 10:57:43 +0100

    testsuite, aarch64, Darwin: Unsorted changes

--- gcc/testsuite/gcc.target/aarch64/pr62308.c
+++ gcc/testsuite/gcc.target/aarch64/pr62308.c
@@ -1,4 +1,5 @@
 /* { dg-do compile } */
+/* { dg-skip-if "Darwin platforms do not support big-endian arm64" *-*-darwin* } */
 /* { dg-options "-mbig-endian" } */
 
 typedef int __attribute__((vector_size(16))) v4si;
