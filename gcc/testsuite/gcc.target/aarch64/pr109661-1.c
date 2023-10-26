b5f643e765faa15a4f0226213e74dcf94ae74dbe
Iain Sandoe <iain@sandoe.co.uk>
2023-10-26 10:57:43 +0100

    testsuite, aarch64, Darwin: Skip tests where the Darwin ABI differs.

--- gcc/testsuite/gcc.target/aarch64/pr109661-1.c
+++ gcc/testsuite/gcc.target/aarch64/pr109661-1.c
@@ -1,4 +1,5 @@
 /* { dg-options "-O2 -Wpsabi" } */
+/* { dg-skip-if "Darwin ABI is different, test separately" { *-*-darwin* } } */
 /* { dg-final { check-function-bodies "**" "" "" } } */
 
 enum __attribute__((aligned(16))) e { E };
