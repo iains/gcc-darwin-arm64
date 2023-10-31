b4238ca829e64f93824d4e1ce36a8e15e77b68ed
Iain Sandoe <iain@sandoe.co.uk>
2023-10-31 16:56:34 +0000

    aarch64: Changes to support the Darwin sub-port.
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-builtins.cc (enum aarch64_builtins):
    	(aarch64_init_subtarget_builtins):
    	* config/aarch64/aarch64-c.cc (aarch64_register_pragmas):
    	* config/aarch64/aarch64-protos.h (enum aarch64_symbol_type):
    	(aarch64_address_valid_for_unscaled_prefetch_p):
    	(aarch64_init_subtarget_builtins):
    	* config/aarch64/aarch64.cc (aarch64_elf_asm_destructor):
    	(aarch64_hard_regno_mode_ok):
    	(aarch64_load_symref_appropriately):
    	(aarch64_expand_mov_immediate):
    	(aarch64_return_in_memory):
    	(aarch64_vfp_is_call_candidate):
    	(aarch64_layout_arg):
    	(aarch64_init_cumulative_args):
    	(aarch64_function_arg_advance):
    	(aarch64_function_arg_regno_p):
    	(aarch64_function_arg_round_boundary):
    	(aarch64_classify_address):
    	(aarch64_address_valid_for_unscaled_prefetch_p):
    	(output_macho_postfix_expr):
    	(aarch64_print_operand):
    	(aarch64_print_address_internal):
    	(aarch64_asm_output_labelref):
    	(aarch64_fold_builtin):
    	(initialize_aarch64_code_model):
    	(aarch64_classify_symbol):
    	(GTY):
    	(aarch64_build_builtin_va_list):
    	(aarch64_expand_builtin_va_start):
    	(aarch64_gimplify_va_arg_expr):
    	(aarch64_setup_incoming_varargs):
    	(aarch64_mangle_type):
    	(aarch64_mov_operand_p):
    	(aarch64_declare_function_name):
    	(aarch64_asm_output_alias):
    	(aarch64_start_file):
    	(aarch64_c_mode_for_suffix):
    	(aarch64_output_simd_mov_immediate):
    	(aarch64_sls_emit_shared_blr_thunks):
    	(TARGET_ASM_UNALIGNED_HI_OP):
    	(TARGET_ASM_UNALIGNED_SI_OP):
    	(TARGET_ASM_UNALIGNED_DI_OP):
    	(TARGET_FUNCTION_ARG_ROUND_BOUNDARY):
    	* config/aarch64/aarch64.h (TARGET_MACHO):
    	(SUBTARGET_EXTRA_SPECS):
    	(EXTRA_SPECS):
    	* config/aarch64/aarch64.md (prefetch_unscaled):
    	* config/aarch64/aarch64.opt:
    	* config/aarch64/constraints.md (Du):
    	* config/aarch64/predicates.md (aarch64_unscaled_prefetch_operand):
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/config/aarch64/aarch64-builtins.cc
+++ gcc/config/aarch64/aarch64-builtins.cc
@@ -888,6 +888,8 @@ enum aarch64_builtins
   AARCH64_BUILTIN_GCSPR,
   AARCH64_BUILTIN_GCSPOPM,
   AARCH64_BUILTIN_GCSSS,
+  /* OS-specific */
+  AARCH64_BUILTIN_CFSTRING,
   AARCH64_BUILTIN_MAX
 };
 
@@ -2483,6 +2485,14 @@ aarch64_general_init_builtins (void)
 
   if (in_lto_p)
     handle_arm_acle_h ();
+}
+
+void
+aarch64_init_subtarget_builtins (void)
+{
+#ifdef SUBTARGET_INIT_BUILTINS
+  SUBTARGET_INIT_BUILTINS;
+#endif
 }
 
 /* Implement TARGET_BUILTIN_DECL for the AARCH64_BUILTIN_GENERAL group.  */
