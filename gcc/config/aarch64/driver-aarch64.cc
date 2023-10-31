bd1583fca135f1f1666cd5baa6e787d0882f36d5
Francois-Xavier Coudert <fxcoudert@gmail.com>
2023-10-31 23:29:46 +0100

    aarch64, Darwin: Add support for -mcpu=native.
    This addresses issue #120.
    
    driver-aarch64: Fix `aarch64-linux => aarch64-apple` cross compile
    
    Because the `aarch64` driver code was previously keying off of only
    `TARGET_MACHO`, it caused this host-side code to be compiled for macOS
    when it could be running on something else, such as an `aarch64` linux
    machine.  This patch changes the conditional to instead key off of
    whether `__APPLE__` is defined, which should be a good indicator that
    the host is actually running on Darwin, and thereby has
    `sysctlbyname()` available to be called.
    
    Native detection of Apple M4 and M5

--- gcc/config/aarch64/driver-aarch64.cc
+++ gcc/config/aarch64/driver-aarch64.cc
@@ -28,6 +28,84 @@
 #include "aarch64-protos.h"
 #include "aarch64-feature-deps.h"
 
+
+/* Default architecture to use if -mcpu=native did not detect a known CPU.  */
+#if TARGET_MACHO
+#define DEFAULT_ARCH "apple-m1"
+#else
+#define DEFAULT_ARCH "8A"
+#define DEFAULT_CPU "generic-armv8-a"
+#endif
+
+
+#if defined(__APPLE__)
+/* macOS does not have /proc/cpuinfo and needs a different approach,
+   based on sysctl.  It is much simpler.  */
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+const char *
+host_detect_local_cpu (ATTRIBUTE_UNUSED int argc, ATTRIBUTE_UNUSED const char **argv)
+{
+  bool arch = false;
+  bool tune = false;
+  bool cpu = false;
+  const char *res = NULL;
+  uint32_t family;
+  size_t len = sizeof(family);
+
+  gcc_assert (argc);
+  if (!argv[0])
+    return NULL;
+
+  /* Are we processing -march, mtune or mcpu?  */
+  arch = strcmp (argv[0], "arch") == 0;
+  if (!arch)
+    tune = strcmp (argv[0], "tune") == 0;
+  if (!arch && !tune)
+    cpu = strcmp (argv[0], "cpu") == 0;
+  if (!arch && !tune && !cpu)
+    return NULL;
+
+  sysctlbyname("hw.cpufamily", &family, &len, NULL, 0);
+
+  switch (family)
+  {
+    case 0x07d34b9f: // Vortex, Tempest
+      res = "apple-a12";
+      break;
+    case 0x573b5eec:
+    case 0x1b588bb3: // Firestorm, Icestorm
+      res = "apple-m1";
+      break;
+    case 0xda33d83d: // Blizzard, Avalanche
+      res = "apple-m2";
+      break;
+    case 0xfa33415e: // Ibiza (M3)
+    case 0x5f4dea93: // Lobos (M3 Pro)
+    case 0x72015832: // Palma (M3 Max)
+      res = "apple-m3";
+      break;
+    case 0x6f5129ac: // Donan (M4)
+    case 0x17d5b93a: // Brava (M4 Pro and Max)
+      res = "apple-m4";
+      break;
+    case 0x1d5a87e8: // Hidra (M5)
+    case 0xf76c5b1a: // Sotra (M5 Pro and Max)
+      res = "apple-m5";
+      break;
+    default:
+      res = DEFAULT_ARCH;
+  }
+
+  if (res)
+    return concat ("-m", argv[0], "=", res, NULL);
+  else
+    return NULL;
+}
+
+#else
+
 struct aarch64_arch_extension
 {
   const char *ext;
@@ -58,9 +136,6 @@ struct aarch64_core_data
 #define INVALID_IMP ((unsigned char) -1)
 #define INVALID_CORE ((unsigned)-1)
 #define ALL_VARIANTS ((unsigned)-1)
-/* Default architecture to use if -mcpu=native did not detect a known CPU.  */
-#define DEFAULT_ARCH "8A"
-#define DEFAULT_CPU "generic-armv8-a"
 
 #define AARCH64_CORE(CORE_NAME, CORE_IDENT, SCHED, ARCH, FLAGS, COSTS, IMP, PART, VARIANT) \
   { CORE_NAME, #ARCH, IMP, PART, unsigned(VARIANT), feature_deps::cpu_##CORE_IDENT },
@@ -516,3 +591,4 @@ not_found:
     return NULL;
   }
 }
+#endif
