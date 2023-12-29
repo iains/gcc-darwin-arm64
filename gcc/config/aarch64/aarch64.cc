6e6065bb0ffa93d1178e6a6e0ed9f1846a28ef6e
Iain Sandoe <iain@sandoe.co.uk>
2023-12-29 11:39:19 +0000

    aarch64, Darwin: Change the PAGEOFF format modifier from K to J.
    Recent upstream backend changes have used K; Shift Darwin to use
    J for this.
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64.cc (aarch64_print_operand): Use J
    	as the print operand modifier for PAGEOFF.
    	* config/aarch64/aarch64.md: Likewise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/config/aarch64/aarch64.cc
+++ gcc/config/aarch64/aarch64.cc
@@ -12463,7 +12463,7 @@ aarch64_print_operand (FILE *f, rtx x, int code)
 	}
       break;
 
-    case 'K':
+    case 'J':
       output_macho_postfix_expr (f, x, "PAGEOFF");
       break;
     case 'O':
