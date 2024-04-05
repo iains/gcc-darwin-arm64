387b52c792b56914376094e080bb8031e968db1f
Iain Sandoe <iain@sandoe.co.uk>
2024-04-05 15:26:02 +0100

    testsuite, aarch64, Darwin: Tests for darwinpcs and calling convention.
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/darwin/aarch64-darwin.exp: New test.
    	* gcc.target/aarch64/darwin/complex-in-regs.c: New test.
    	* gcc.target/aarch64/darwin/darwinpcs-d1.c: New test.
    	* gcc.target/aarch64/darwin/darwinpcs-d2-00.c: New test.
    	* gcc.target/aarch64/darwin/darwinpcs-d2-01.c: New test.
    	* gcc.target/aarch64/darwin/darwinpcs-d2-02.c: New test.
    	* gcc.target/aarch64/darwin/darwinpcs-d2-03.c: New test.
    	* gcc.target/aarch64/darwin/darwinpcs-d2-04.c: New test.
    	* gcc.target/aarch64/darwin/darwinpcs-d3.c: New test.
    	* gcc.target/aarch64/darwin/darwinpcs-d4.c: New test.
    	* gcc.target/aarch64/darwin/float128-00.c: New test.
    	* gcc.target/aarch64/darwin/homogeneous-aggr.c: New test.
    	* gcc.target/aarch64/darwin/k+r-00.c: New test.
    	* gcc.target/aarch64/darwin/tu-accesses-0.c: New test.
    	* gcc.target/aarch64/darwin/variadic-00.c: New test.
    	* gcc.target/aarch64/darwin/variadic-01.c: New test.
    	* gcc.target/aarch64/darwin/variadic-02.c: New test.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

---
+++ gcc/testsuite/gcc.target/aarch64/darwin/darwinpcs-d3.c
@@ -0,0 +1,40 @@
+/* { dg-do compile } */
+/* { dg-additional-options "-O " } */
+/* { dg-final { check-function-bodies "**" "" "" { xfail *-*-darwin* } } } */
+
+/* This will fail, because of issue #74
+**foo: 
+**	cmp	w0, w1
+**	cset	w0, eq
+**	ret
+*/
+
+__attribute__((__noinline__))
+int
+foo (char a, unsigned char b)
+{
+  return a == b ? 1 : 0;
+}
+
+__attribute__((__noinline__))
+int
+bar (short a, unsigned short b)
+{
+  return a == b ? 1 : 0;
+}
+
+void pop (char *, unsigned char *, short *, unsigned short *);
+
+int main ()
+{
+   char a;
+   unsigned char b;
+   short c;
+   unsigned short d;
+   int result;
+   pop (&a, &b, &c, &d);
+   
+   result = foo (a, b);
+   result += bar (c, d);
+   return result;
+}
