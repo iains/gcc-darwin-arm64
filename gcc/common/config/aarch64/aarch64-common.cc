2343391158335c84c8a0e7423b7a0fa48512f68b
Iain Sandoe <iain@sandoe.co.uk>
2024-04-14 07:07:38 +0100

    aarch64: Emit +rdm when the platform asm does not support +rdma.
    Although the two extension names are aliases, they are not supported
    uniformly across assemblers.
    
    This emits +rdm for assemblers that do not support +rdma.
    
    TODO: Wrap the change in a conditional and supply that from config.gcc
    on the basis of configuration.
    
    gcc/ChangeLog:
    
    	* common/config/aarch64/aarch64-common.cc: Always emit +rdm
    	even when the given extension name is rdma.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/common/config/aarch64/aarch64-common.cc
+++ gcc/common/config/aarch64/aarch64-common.cc
@@ -694,7 +694,10 @@ aarch64_get_extension_string_for_isa_flags
     if (added & opt.flag_canonical)
       {
 	outstr += "+";
-	outstr += opt.name;
+	if (startswith (opt.name, "rdm"))
+	  outstr += "rdm";
+	else
+	  outstr += opt.name;
       }
 
   /* Remove the features in current_flags & ~isa_flags.  If the feature does
@@ -736,7 +739,10 @@ aarch64_get_extension_string_for_isa_flags
     if (removed & opt.flag_canonical)
       {
 	outstr += "+no";
-	outstr += opt.name;
+	if (startswith (opt.name, "rdm"))
+	  outstr += "rdm";
+	else
+	  outstr += opt.name;
       }
 
   return outstr;
