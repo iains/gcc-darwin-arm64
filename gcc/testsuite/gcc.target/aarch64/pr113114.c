4c1f8a87bdb2c95fee2d40136a83ea89e3a5bc9f
Iain Sandoe <iain@sandoe.co.uk>
2024-05-05 14:20:10 +0100

    testsuite, aarch64, darwin: Skip two tests relying on ILP32.
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/gcc.target/aarch64/pr113114.c
+++ gcc/testsuite/gcc.target/aarch64/pr113114.c
@@ -1,5 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-mabi=ilp32 -O -mearly-ldp-fusion -mlate-ldp-fusion" } */
+/* { dg-skip-if "-mabi=ilp32 unsupported" { aarch64-*-darwin* } } */
 void foo_n(double *a) {
   int i = 1;
   for (; i < (int)foo_n; i++)
