0e1de0a0ca8ab9365f5bd161efea5ce4f6338b8b
Iain Sandoe <iain@sandoe.co.uk>
2024-10-14 17:16:35 +0100

    c-family: Advertise the availability attribute support in features.
    This is a temporary workaround - we are going to advertise the availablility
    attribute as present for all targets - which it is not.  For the Darwin
    branch it is a short-term band-aid.
    
    This addresses Issue #12 part 2.
    
    Signed-off-by: Iain Sandoe <iains@gcc.gnu.org>

--- gcc/c-family/c-common.cc
+++ gcc/c-family/c-common.cc
@@ -354,7 +354,11 @@ static constexpr hf_feature_info has_feature_table[] =
   { "enumerator_attributes",		  HF_FLAG_NONE, 0 },
   { "tls",				  HF_FLAG_NONE, 0 },
   { "gnu_asm_goto_with_outputs",	  HF_FLAG_EXT, 0 },
-  { "gnu_asm_goto_with_outputs_full",	  HF_FLAG_EXT, 0 }
+  { "gnu_asm_goto_with_outputs_full",	  HF_FLAG_EXT, 0 },
+  { "attribute_availability",		  HF_FLAG_NONE, 0 },
+  { "attribute_availability_with_message",  HF_FLAG_NONE, 0 },
+  { "attribute_availability_with_replacement",  HF_FLAG_NONE, 0 },
+  { "attribute_availability_with_version_underscores",  HF_FLAG_NONE, 0 }
 };
 
 /* Global visibility options.  */
