d163254411f5be01d05a483d22ffca2eca14dfbd
Iain Sandoe <iain@sandoe.co.uk>
2026-06-13 17:16:55 +0100

    testsuite, aarch64, Darwin: Updates to advsimd-intrinsics tests.
    Adding global options in the advsimd-intrinsics.exp does not work when the
    current target happens to support more advanced features without additional
    options; what happens is the the global top level ones get used and they are
    not adequate for some tests.  I don't see an easy way around this since the
    checks for target options do not take those global options into account.
    
    Seveal tests are 'assemble' which does not cause an ltrans.s file to be
    generated leading to UNRESOLVED tests when -flot is passed.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/advsimd-intrinsics/advsimd-intrinsics.exp:
    	* gcc.target/aarch64/advsimd-intrinsics/bf16_get.c:
    	* gcc.target/aarch64/advsimd-intrinsics/bfcvt-compile.c:
    	* gcc.target/aarch64/advsimd-intrinsics/bfcvt-nosimd.c:
    	* gcc.target/aarch64/advsimd-intrinsics/bfcvtnq2-untied.c:
    	* gcc.target/aarch64/advsimd-intrinsics/bfdot-1.c:
    	* gcc.target/aarch64/advsimd-intrinsics/combine.c:
    	* gcc.target/aarch64/advsimd-intrinsics/vdot-3-1.c:
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/gcc.target/aarch64/advsimd-intrinsics/bfcvtnq2-untied.c
+++ gcc/testsuite/gcc.target/aarch64/advsimd-intrinsics/bfcvtnq2-untied.c
@@ -3,6 +3,7 @@
 /* { dg-add-options arm_v8_2a_bf16_neon } */
 /* { dg-additional-options "-save-temps" } */
 /* { dg-final { check-function-bodies "**" "" {-O[^0]} } } */
+/* { dg-skip-if "assemble does not produce ltrans file" { *-*-darwin* } { "-flto" } } */
 /* { dg-skip-if "" { *-*-* } { "-fno-fat-lto-objects" } } */
 
 #include <arm_neon.h>
