36692b91715693f3c37c7ef51b8b67bfecf8c4b5
Iain Sandoe <iain@sandoe.co.uk>
2026-06-15 14:14:52 +0100

    testsuite: Add multiple scan introducers to check-function-bodies.
    The motivation for this is that it is not unusal for subtargets to have
    substantially equivalent code-gen but differing in details.  This provision
    avoid duplication of the common sections.
    
    There are two cases:
      1. the check-function-bodies directive omits or specifies the scan introducer
         as "" or "**".  This is treated as "do the default", and we construct a list
         that is currently { ** *X } where X depends on the object format: E for ELF
         P for PE/COFF and M for Mach-O
      2. the directive specifies a list.  In this case we assume that the user needs
         something special and just keep that list verbatim.
    
    but this now potentially simplifies multi-object format directives:
    
    Viz:
    // { dg-final { check-function-bodies ""  "" "" { target *-*-* } {\.L[0-9]+} } }
    
    thus we can make match specs like:
    
    /*
    ** far_branch_u16_x0_eq_42:
    **	sub	sp, sp, #16
    *E	mov	w([0-9]+), 42
    **	str	wzr, \[sp, 12\]
    *E	cbheq	w0, w\1, .L([0-9]+)
    *M	cbeq	w0, 42, .L([0-9]+)
    **	b	.L([0-9]+)
    *E .L\2:
    *M .L\1:
    **	str	wzr, \[sp, 12\]
    **	...
    **	str	wzr, \[sp, 12\]
    *E .L\3:
    *M .L\2:
    **	add	sp, sp, 16
    **	b	_?taken
    */
    
    Which is more efficient than duplication of that entire block (with a
    different introducer).
    
    gcc/testsuite/ChangeLog:
    
    	* lib/scanasm.exp: Allow multiple scan line introducers.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/gcc.target/i386/memcpy-pr120683-1.c
+++ gcc/testsuite/gcc.target/i386/memcpy-pr120683-1.c
@@ -1,7 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -mno-sse -mmemcpy-strategy=unrolled_loop:256:noalign,libcall:-1:noalign" } */
-/* { dg-final { check-function-bodies "**" "*#" "" { target { lp64 && { ! *-*-darwin* } } } {^\t?\.} } } */
-/* { dg-final { check-function-bodies "*D" "*E" "" { target { lp64 && *-*-darwin* } } {^\t?\.} } } */
+/* { dg-final { check-function-bodies "" "" "" { target lp64 } {^\t?\.} } } */
 
 /*
 **foo:
@@ -20,6 +19,7 @@
 **	movl	%edx, %eax
 **	addl	\$32, %edx
 **	movq	\(%rsi,%rax\), %r10
+*M	cmpl	\$224, %edx
 **	movq	8\(%rsi,%rax\), %r9
 **	movq	16\(%rsi,%rax\), %r8
 **	movq	24\(%rsi,%rax\), %rcx
@@ -27,38 +27,10 @@
 **	movq	%r9, 8\(%rdi,%rax\)
 **	movq	%r8, 16\(%rdi,%rax\)
 **	movq	%rcx, 24\(%rdi,%rax\)
-**	cmpl	\$224, %edx
+*E	cmpl	\$224, %edx
 **	jb	.L[0-9]+
 **	ret
 **...
-*#
-
-*Dfoo:
-*D	movq	221\(%rsi\), %rax
-*D	xorl	%edx, %edx
-*D	movq	%rax, 221\(%rdi\)
-*D	movq	229\(%rsi\), %rax
-*D	movq	%rax, 229\(%rdi\)
-*D	movq	237\(%rsi\), %rax
-*D	movq	%rax, 237\(%rdi\)
-*D	movq	245\(%rsi\), %rax
-*D	movq	%rax, 245\(%rdi\)
-*D.L[0-9]+:
-*D	movl	%edx, %eax
-*D	addl	\$32, %edx
-*D	movq	\(%rsi,%rax\), %r10
-*D	cmpl	\$224, %edx
-*D	movq	8\(%rsi,%rax\), %r9
-*D	movq	16\(%rsi,%rax\), %r8
-*D	movq	24\(%rsi,%rax\), %rcx
-*D	movq	%r10, \(%rdi,%rax\)
-*D	movq	%r9, 8\(%rdi,%rax\)
-*D	movq	%r8, 16\(%rdi,%rax\)
-*D	movq	%rcx, 24\(%rdi,%rax\)
-*D	jb	.L[0-9]+
-*D	ret
-*D...
-*E
 */
 
 void
