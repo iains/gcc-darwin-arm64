61d798c2d3cbfd5b397676cf4d0b7afdf7a44fd1
Iain Sandoe <iain@sandoe.co.uk>
2026-07-15 18:17:50 +0100

    testsuite, aarch64: Require target support for tiny and large models.
    It is not enough just to add the -mcmodel= option, since sub-targets might
    not support the specified model.  So add the option and then check that the
    support is present using _nocache versions so that they act on the test
    flags.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/aarch64/pr70120-2.c: Require mcmodel support.
    	* gcc.target/aarch64/pr78255.c: Likewise.
    	* gcc.target/aarch64/pr78561.c: Likewise.
    	* gcc.target/aarch64/pr78733.c: Likewise.
    	* gcc.target/aarch64/pr79041-2.c: Likewise.
    	* gcc.target/aarch64/pr94530.c: Likewise.
    	* gcc.target/aarch64/reload-valid-spoff.c: Likewise.
    	* gcc.target/aarch64/symbol-range-tiny.c: Likewise.
    
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>

--- gcc/testsuite/gcc.target/aarch64/reload-valid-spoff.c
+++ gcc/testsuite/gcc.target/aarch64/reload-valid-spoff.c
@@ -2,6 +2,7 @@
 /* { dg-require-effective-target lp64 } */
 /* { dg-options "-O2 -mcmodel=large -fno-builtin -fno-pie" }  */
 /* { dg-skip-if "-mcmodel=large -fPIC not currently supported" { aarch64-*-* }  { "-fPIC" } { "" } } */
+/* { dg-require-effective-target aarch64_large_nocache } */
 
 typedef long unsigned int size_t;
 typedef unsigned short int sa_family_t;
